8e7929faf5a1d18c4e019bad369c876ddd07b546
jamison904 <james.jamison85@gmail.com>
2014-01-24 19:23:05 -0500

    BioShock 4.0

--- build_master.sh
+++ build_master.sh
@@ -2,22 +2,18 @@
 export PLATFORM="TW"
 export MREV="JB4.3"
 export CURDATE=`date "+%m.%d.%Y"`
-export MUXEDNAMELONG="KT-SGS4-$MREV-$PLATFORM-$CARRIER-$CURDATE"
-export MUXEDNAMESHRT="KT-SGS4-$MREV-$PLATFORM-$CARRIER*"
-export KTVER="--$MUXEDNAMELONG--"
+export MUXEDNAMELONG="_BioShock_4.0-$MREV-$PLATFORM-$CARRIER-$CURDATE"
+export MUXEDNAMESHRT="_BioShock_4.0-$MREV-$PLATFORM-$CARRIER*"
+export KTVER="_BioShock_4.0-"
 export KERNELDIR=`readlink -f .`
 export PARENT_DIR=`readlink -f ..`
 export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
 export INITRAMFS_SOURCE=`readlink -f ..`/Ramdisks/$PLATFORM"_"$CARRIER"4.3"
 export CONFIG_$PLATFORM_BUILD=y
-export PACKAGEDIR=$PARENT_DIR/Packages/$PLATFORM
+export PACKAGEDIR=/home/jamison/android/kernel/out/Packages/$PLATFORM
 #Enable FIPS mode
 export USE_SEC_FIPS_MODE=true
 export ARCH=arm
-# export CROSS_COMPILE=$PARENT_DIR/linaro4.5/bin/arm-eabi-
-# export CROSS_COMPILE=/home/ktoonsez/kernel/siyah/arm-2011.03/bin/arm-none-eabi-
-# export CROSS_COMPILE=/home/ktoonsez/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-# export CROSS_COMPILE=/home/ktoonsez/aokp4.2/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
 export CROSS_COMPILE=$PARENT_DIR/linaro4.7/bin/arm-eabi-
 
 time_start=$(date +%s.%N)
@@ -52,57 +48,8 @@ echo "Make the kernel"
 make VARIANT_DEFCONFIG=jf_$CARRIER"_defconfig" SELINUX_DEFCONFIG=jfselinux_defconfig SELINUX_LOG_DEFCONFIG=jfselinux_log_defconfig KT_jf_defconfig
 
 echo "Modding .config file - "$KTVER
-sed -i 's,CONFIG_LOCALVERSION="-KT-SGS4",CONFIG_LOCALVERSION="'$KTVER'",' .config
+sed -i 's,CONFIG_LOCALVERSION="_BioShock_4.0-",CONFIG_LOCALVERSION="'$KTVER'",' .config
 
-HOST_CHECK=`uname -n`
-if [ $HOST_CHECK = 'ktoonsez-VirtualBox' ] || [ $HOST_CHECK = 'task650-Underwear' ]; then
-	echo "Ktoonsez/task650 24!"
-	make -j24
-else
-	echo "Others! - " + $HOST_CHECK
-	make -j`grep 'processor' /proc/cpuinfo | wc -l`
-fi;
+	make -j5
 
-echo "Copy modules to Package"
-cp -a $(find . -name *.ko -print |grep -v initramfs) $PACKAGEDIR/system/lib/modules/
-if [ $ADD_KTWEAKER = 'Y' ]; then
-	cp /home/ktoonsez/workspace/com.ktoonsez.KTweaker.apk $PACKAGEDIR/system/app/com.ktoonsez.KTweaker.apk
-fi;
 
-if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
-	echo "Copy zImage to Package"
-	cp arch/arm/boot/zImage $PACKAGEDIR/zImage
-
-	echo "Make boot.img"
-	./mkbootfs $INITRAMFS_DEST | gzip > $PACKAGEDIR/ramdisk.gz
-	./mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $PACKAGEDIR/boot.img 
-	if [ $EXEC_LOKI = 'Y' ]; then
-		echo "Executing loki"
-		./loki_patch-linux-x86_64 boot aboot$CARRIER.img $PACKAGEDIR/boot.img $PACKAGEDIR/boot.lok
-		rm $PACKAGEDIR/boot.img
-	fi;
-	cd $PACKAGEDIR
-	if [ $EXEC_LOKI = 'Y' ]; then
-		cp -R ../META-INF-SEC ./META-INF
-	else
-		cp -R ../META-INF .
-	fi;
-	rm ramdisk.gz
-	rm zImage
-	rm ../$MUXEDNAMESHRT.zip
-	zip -r ../$MUXEDNAMELONG.zip .
-
-	time_end=$(date +%s.%N)
-	echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
-
-	FILENAME=../$MUXEDNAMELONG.zip
-	FILESIZE=$(stat -c%s "$FILENAME")
-	echo "Size of $FILENAME = $FILESIZE bytes."
-	rm ../$MREV-$PLATFORM-$CARRIER"-version.txt"
-	exec >>../$MREV-$PLATFORM-$CARRIER"-version.txt" 2>&1
-	echo "$MUXEDNAMELONG,$FILESIZE,http://ktoonsez.jonathanjsimon.com/sgs4/$PLATFORM/$MUXEDNAMELONG.zip"
-	
-	cd $KERNELDIR
-else
-	echo "KERNEL DID NOT BUILD! no zImage exist"
-fi;
